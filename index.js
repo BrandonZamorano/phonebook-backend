571008a53889e0002500446571dedde81db842ec
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-11-17 16:20:54 -0600

    finish 3.11 phonebook full stack

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 morgan.token('postData', (request, response) => JSON.stringify(request.body))
 
+app.use(express.static("build"))
 app.use(cors());
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))

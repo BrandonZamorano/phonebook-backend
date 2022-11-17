8a7c34475da5214fd9f3b3783d3a4c4d7bfda2f6
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-11-17 13:19:13 -0600

    Finish 3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express();
 const PORT = 3001;
 
 morgan.token('postData', (request, response) => JSON.stringify(request.body))
 
+app.use(cors());
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 

b7e89ea72043760b2874e82ca17e8b2ea45162dd
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-11-18 14:48:01 -0600

    finish 3.21 Deploying the database backend to production

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post("/api/persons", (request, response, next) => {
                 person.save()
                     .then(savedPerson => {
                         response.json(savedPerson)
-                    })
+                    }).catch(error => next(error))
             }
         })
         .catch(error => next(error))
